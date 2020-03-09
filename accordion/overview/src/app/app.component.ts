34d829d1e910ee41a64e8a4c811cd7027869b175
bmarkov <boikom@jqwidgets.com>
2020-03-09 17:08:01 +0200

    Add files via upload

---
+++ accordion/overview/src/app/app.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component, ViewChild } from '@angular/core';
+import { AccordionComponent, AccordionItemComponent, AccordionExpandMode } from 'smart-webcomponents-angular/accordion';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+    @ViewChild('accordion', { read: AccordionComponent, static: false }) accordion: AccordionComponent;
+
+    ngOnInit(): void {
+
+    }
+    ngAfterViewInit(): void {
+		const that = this;
+		that.accordion.expandMode = AccordionExpandMode.Multiple;
+		that.accordion.expand(1);  	
+    }
+}
