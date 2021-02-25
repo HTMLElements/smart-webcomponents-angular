032bc876ffbe4ceb67829741ee80a225c6ce1ca9
bmarkov <boikom@jqwidgets.com>
2021-02-25 08:35:49 -0800

    Update app.component.ts

--- accordion/overview/src/app/app.component.ts
+++ accordion/overview/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AccordionComponent, AccordionItemComponent, AccordionExpandMode } from 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: './app.component.css'
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
