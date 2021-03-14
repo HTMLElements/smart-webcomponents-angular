58de41a418fa974baf4aa63c95611bebeacd9ac5
bmarkov <boikom@jqwidgets.com>
2021-03-14 16:27:03 +0200

    Update app.module.ts

--- accordion/overview/src/app/app.module.ts
+++ accordion/overview/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AccordionModule } from 'smart-webcomponents-angular-dev/accordion';
+import { AccordionModule } from '@smart-webcomponents-angular/accordion';
 
 import { AppComponent } from './app.component';
 
