df15447a0242a7b388cdcecf7a11689714313c2d
bmarkov <boikom@jqwidgets.com>
2021-04-14 14:31:53 +0300

    abc

--- demos/colorpicker/forms/src/app/app.module.ts
+++ demos/colorpicker/forms/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { ColorPickerModule } from '@smart-webcomponents-angular/colorpicker';
 import { DropDownListModule } from '@smart-webcomponents-angular/dropdownlist';
+import { InputModule } from '@smart-webcomponents-angular/input';
+import { ButtonModule } from '@smart-webcomponents-angular/button';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, FormsModule, DropDownListModule, ColorPickerModule],
+    imports: [BrowserModule, FormsModule, InputModule, ButtonModule, DropDownListModule, ColorPickerModule],
     bootstrap: [AppComponent],
     entryComponents: [AppComponent]
 })
