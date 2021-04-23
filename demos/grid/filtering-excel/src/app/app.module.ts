03781368abd01367ca62ef1598e92da6b679dece
bmarkov <boikom@jqwidgets.com>
2021-04-23 18:18:54 +0300

    up

--- demos/grid/filtering-excel/src/app/app.module.ts
+++ demos/grid/filtering-excel/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { GridModule } from '@smart-webcomponents-angular/grid';
+import { TreeModule } from '@smart-webcomponents-angular/tree';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [ AppComponent ],
-    imports: [ BrowserModule, GridModule ],
+    imports: [ BrowserModule, GridModule, TreeModule ],
     bootstrap: [ AppComponent ],
 	entryComponents: [ AppComponent ]
 })
