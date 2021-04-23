0778f06df304b9ab397a87d9f688b12ffa2de6f5
bmarkov <boikom@jqwidgets.com>
2021-04-23 17:40:22 +0300

    up

--- demos/grid/editing-cascading/src/app/app.module.ts
+++ demos/grid/editing-cascading/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { GridModule } from '@smart-webcomponents-angular/grid';
+import { DropDownListModule } from '@smart-webcomponents-angular/dropdownlist';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, GridModule],
+    imports: [BrowserModule, GridModule, DropDownListModule],
     bootstrap: [AppComponent],
     entryComponents: [AppComponent]
 })
