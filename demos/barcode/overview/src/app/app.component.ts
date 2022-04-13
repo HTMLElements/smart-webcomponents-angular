218af71929cd6ecff2936d52c546bbb6bbebea32
bmarkov <boikom@jqwidgets.com>
2022-04-13 09:53:13 +0300

    barcode and qrcode

---
+++ demos/barcode/overview/src/app/app.component.ts
@@ -0,0 +1,41 @@
+﻿import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { ButtonComponent } from '@smart-webcomponents-angular/button';
+import { BarcodeComponent } from '@smart-webcomponents-angular/barcode';
+
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements AfterViewInit, OnInit {	
+	@ViewChild('barcode', { read: BarcodeComponent, static: false }) barcode: BarcodeComponent;
+	@ViewChild('barcode2', { read: BarcodeComponent, static: false }) barcode2: BarcodeComponent;
+	@ViewChild('barcode3', { read: BarcodeComponent, static: false }) barcode3: BarcodeComponent;
+	@ViewChild('barcode4', { read: BarcodeComponent, static: false }) barcode4: BarcodeComponent;
+	@ViewChild('barcode5', { read: BarcodeComponent, static: false }) barcode5: BarcodeComponent;
+	@ViewChild('barcode6', { read: BarcodeComponent, static: false }) barcode6: BarcodeComponent;
+	@ViewChild('barcode7', { read: BarcodeComponent, static: false }) barcode7: BarcodeComponent;
+	@ViewChild('barcode8', { read: BarcodeComponent, static: false }) barcode8: BarcodeComponent;
+	@ViewChild('barcode9', { read: BarcodeComponent, static: false }) barcode9: BarcodeComponent;
+	@ViewChild('barcode10', { read: BarcodeComponent, static: false }) barcode10: BarcodeComponent;
+	@ViewChild('barcode11', { read: BarcodeComponent, static: false }) barcode11: BarcodeComponent;
+	@ViewChild('barcode12', { read: BarcodeComponent, static: false }) barcode12: BarcodeComponent;
+	@ViewChild('barcode13', { read: BarcodeComponent, static: false }) barcode13: BarcodeComponent;
+
+	ngOnInit(): void {
+		// onInit code.
+	}
+
+	ngAfterViewInit(): void {
+		// afterViewInit code.
+		this.init();
+    }
+		
+	init(): void {
+		// init code.
+	    
+
+	}	
+}
---
+++ demos/qrcode/overview/src/app/app.component.ts
@@ -0,0 +1,41 @@
+﻿import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { ButtonComponent } from '@smart-webcomponents-angular/button';
+import { QRcodeComponent } from '@smart-webcomponents-angular/qrcode';
+
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements AfterViewInit, OnInit {	
+	@ViewChild('qrcode', { read: QRcodeComponent, static: false }) qrcode: QRcodeComponent;
+	@ViewChild('qrcode2', { read: QRcodeComponent, static: false }) qrcode2: QRcodeComponent;
+	@ViewChild('qrcode3', { read: QRcodeComponent, static: false }) qrcode3: QRcodeComponent;
+	@ViewChild('qrcode4', { read: QRcodeComponent, static: false }) qrcode4: QRcodeComponent;
+	@ViewChild('qrcode5', { read: QRcodeComponent, static: false }) qrcode5: QRcodeComponent;
+	@ViewChild('qrcode6', { read: QRcodeComponent, static: false }) qrcode6: QRcodeComponent;
+	@ViewChild('qrcode7', { read: QRcodeComponent, static: false }) qrcode7: QRcodeComponent;
+	@ViewChild('qrcode8', { read: QRcodeComponent, static: false }) qrcode8: QRcodeComponent;
+	@ViewChild('qrcode9', { read: QRcodeComponent, static: false }) qrcode9: QRcodeComponent;
+	@ViewChild('qrcode10', { read: QRcodeComponent, static: false }) qrcode10: QRcodeComponent;
+	@ViewChild('qrcode11', { read: QRcodeComponent, static: false }) qrcode11: QRcodeComponent;
+	@ViewChild('qrcode12', { read: QRcodeComponent, static: false }) qrcode12: QRcodeComponent;
+	@ViewChild('qrcode13', { read: QRcodeComponent, static: false }) qrcode13: QRcodeComponent;
+
+	ngOnInit(): void {
+		// onInit code.
+	}
+
+	ngAfterViewInit(): void {
+		// afterViewInit code.
+		this.init();
+    }
+		
+	init(): void {
+		// init code.
+	    
+
+	}	
+}
