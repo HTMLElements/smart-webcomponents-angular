476555920d822d8198d472cd1dca13644814b783
bmarkov <boikom@jqwidgets.com>
2023-01-27 14:53:08 +0200

    Update app.module.ts

--- demos/scheduler/restricted-dates/src/app/app.module.ts
+++ demos/scheduler/restricted-dates/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { SchedulerModule } from '@smart-webcomponents-angular/scheduler';
+import { DateTimePickerModule } from '@smart-webcomponents-angular/datetimepicker';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [ AppComponent ],
-    imports: [ BrowserModule, SchedulerModule ],
+    imports: [ BrowserModule, DateTimePickerModule, SchedulerModule ],
     bootstrap: [ AppComponent ],
 	entryComponents: [ AppComponent ]
 })
