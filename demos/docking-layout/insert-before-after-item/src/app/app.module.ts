885e2df15211ca7022cfba92756f9d8cc02c92d1
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-17 16:11:53 +0200

    fix: DockingLayout Insert Before After Item demo

--- demos/docking-layout/insert-before-after-item/src/app/app.module.ts
+++ demos/docking-layout/insert-before-after-item/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from '@smart-webcomponents-angular/button';
 import { DockingLayoutModule } from '@smart-webcomponents-angular/dockinglayout';
 import { DropDownListModule } from '@smart-webcomponents-angular/dropdownlist';
+import { WindowModule } from '@smart-webcomponents-angular/window';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [ AppComponent ],
-    imports: [ BrowserModule, ButtonModule, DockingLayoutModule, DropDownListModule ],
+    imports: [ BrowserModule, ButtonModule, DockingLayoutModule, DropDownListModule, WindowModule ],
     bootstrap: [ AppComponent ],
 	entryComponents: [ AppComponent ]
 })
