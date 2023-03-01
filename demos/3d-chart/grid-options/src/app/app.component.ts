bdbd6bded0388aa606cd1a2e41db00f8bbb14845
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-03-01 07:39:18 +0200

    fix: 3d Chart Grid Options demo

--- demos/3d-chart/grid-options/src/app/app.component.ts
+++ demos/3d-chart/grid-options/src/app/app.component.ts
@@ -25,33 +25,93 @@ export class AppComponent implements AfterViewInit, OnInit {
   dataSource = [
     {
       Day: "Monday",
-      Keith: 20,
-      Erica: 30,
-      George: 70,
+      Keith: {
+        Running: 10,
+        Swimming: 20,
+        Cycling: 30,
+      },
+      Erica: {
+        Running: 13,
+        Swimming: 30,
+        Cycling: 23,
+      },
+      George: {
+        Running: 15,
+        Swimming: 70,
+        Cycling: 10,
+      },
     },
     {
       Day: "Tuesday",
-      Keith: 15,
-      Erica: 40,
-      George: 35,
+      Keith: {
+        Running: 15,
+        Swimming: 15,
+        Cycling: 40,
+      },
+      Erica: {
+        Running: 10,
+        Swimming: 15,
+        Cycling: 40,
+      },
+      George: {
+        Running: 35,
+        Swimming: 15,
+        Cycling: 40,
+      },
     },
     {
       Day: "Wednesday",
-      Keith: 55,
-      Erica: 60,
-      George: 5
+      Keith: {
+        Running: 55,
+        Swimming: 15,
+        Cycling: 10,
+      },
+      Erica: {
+        Running: 15,
+        Swimming: 60,
+        Cycling: 5,
+      },
+      George: {
+        Running: 50,
+        Swimming: 5,
+        Cycling: 10,
+      },
     },
     {
       Day: "Thursday",
-      Keith: 15,
-      Erica: 35,
-      George: 5,
+      Keith: {
+        Running: 15,
+        Swimming: 15,
+        Cycling: 40,
+      },
+      Erica: {
+        Running: 15,
+        Swimming: 15,
+        Cycling: 40,
+      },
+      George: {
+        Running: 15,
+        Swimming: 15,
+        Cycling: 5,
+      },
     },
     {
       Day: "Friday",
-      Keith: 40,
-      Erica: 30,
-      George: 50,
+      Keith: {
+        Running: 15,
+        Swimming: 15,
+        Cycling: 40,
+      },
+      Erica: {
+        Running: 15,
+        Swimming: 15,
+        Cycling: 40,
+      },
+      George: {
+        Running: 5,
+        Swimming: 5,
+        Cycling: 40,
+      },
     },
   ];
 
@@ -74,18 +134,65 @@ export class AppComponent implements AfterViewInit, OnInit {
   };
 
   valueAxis = {
-    unitInterval: 5,
     minValue: 0,
   }
 
   seriesGroups = [
     {
-      type: "column",
-
+      type: "stackedcolumn",
+      dataField: "Keith",
+      displayText: "Keith",
       series: [
-        { dataField: "Keith", displayText: "Keith" },
-        { dataField: "Erica", displayText: "Erica" },
-        { dataField: "George", displayText: "George" },
+        {
+          dataField: "Running",
+          displayText: "Running",
+        },
+        {
+          dataField: "Cycling",
+          displayText: "Cycling",
+        },
+        {
+          dataField: "Swimming",
+          displayText: "Swimming",
+        },
+      ],
+    },
+    {
+      type: "stackedcolumn",
+      dataField: "Erica",
+      displayText: "Erica",
+      series: [
+        {
+          dataField: "Running",
+          displayText: "Running",
+        },
+        {
+          dataField: "Cycling",
+          displayText: "Cycling",
+        },
+        {
+          dataField: "Swimming",
+          displayText: "Swimming",
+        },
+      ],
+    },
+    {
+      type: "stackedcolumn",
+      dataField: "George",
+      displayText: "George",
+      series: [
+        {
+          dataField: "Running",
+          displayText: "Running",
+        },
+        {
+          dataField: "Cycling",
+          displayText: "Cycling",
+        },
+        {
+          dataField: "Swimming",
+          displayText: "Swimming",
+        },
       ],
     },
   ];
