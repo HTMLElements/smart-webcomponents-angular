f5d67b83d143661d55242d742bc745c1fe1edc37
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-03-21 07:20:43 +0200

    Update app.component.ts

--- demos/framework/html-checkbox-input-binding/src/app/app.component.ts
+++ demos/framework/html-checkbox-input-binding/src/app/app.component.ts
@@ -1,38 +1,38 @@
 ﻿import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 
+import 'smart-webcomponents-angular/element';
+
 import 'smart-webcomponents-angular/source/smart.element.js';
 import 'smart-webcomponents-angular/source/smart.core.js';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent implements AfterViewInit, OnInit {
-	
-	app = new window.Smart.App({
-		data: {
-			isChecked: true
-		}
-	});
+  app = new window.Smart.App({
+    data: {
+      isChecked: true,
+    },
+  });
 
-	ngOnInit(): void {
-		// onInit code.
-	}
+  ngOnInit(): void {
+    // onInit code.
+  }
 
-	ngAfterViewInit(): void {
-		// afterViewInit code.
-		this.init();
-	}
+  ngAfterViewInit(): void {
+    // afterViewInit code.
+    this.init();
+  }
 
-	init(): void {
-		// init code.
+  init(): void {
+    // init code.
 
-		this.app = new window.Smart.App({
-			data: {
-				isChecked: true
-			}
-		});
-	}
+    this.app = new window.Smart.App({
+      data: {
+        isChecked: true,
+      },
+    });
+  }
 }
