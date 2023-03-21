f16731ecb674b4c41c214652a8beb44e4b0c365a
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-03-21 07:39:16 +0200

    fix: Init from app demo

--- demos/framework/init-from-app/src/app/app.component.ts
+++ demos/framework/init-from-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import { GridComponent, GridColumn, DataAdapter, Smart } from '@smart-webcomponents-angular/grid';
 import { DropDownListComponent } from '@smart-webcomponents-angular/dropdownlist';
-import { GetData } from '../../common/data';
+import { GetData } from '../assets/data';
 
 import 'smart-webcomponents-angular/source/smart.core.js';
 
