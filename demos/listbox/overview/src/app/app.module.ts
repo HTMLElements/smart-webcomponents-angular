b1f0bf92efa21c61191db5f09fbc4990c30fc50c
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-03-24 10:48:39 +0200

    fix: Listbox Overview demo

--- demos/listbox/overview/src/app/app.module.ts
+++ demos/listbox/overview/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { ButtonModule } from '@smart-webcomponents-angular/button';import { ListBoxModule } from '@smart-webcomponents-angular/listbox';
+import { ButtonModule } from '@smart-webcomponents-angular/button';
+import { ListBoxModule } from '@smart-webcomponents-angular/listbox';
+import { SliderModule } from '@smart-webcomponents-angular/slider';
+import { SwitchButtonModule } from '@smart-webcomponents-angular/switchbutton';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [ AppComponent ],
-    imports: [ BrowserModule, ButtonModule, ListBoxModule ],
+    imports: [ BrowserModule, ButtonModule, ListBoxModule, SliderModule, SwitchButtonModule ],
     bootstrap: [ AppComponent ],
 	entryComponents: [ AppComponent ]
 })
