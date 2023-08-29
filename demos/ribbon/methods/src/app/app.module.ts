d18bb7fbd9597cf94e881a1d7d990d4860d37d2a
IvanPeevski <37153196+IvanPeevski@users.noreply.github.com>
2023-08-29 13:08:26 +0300

    up ribbon demos

--- demos/ribbon/methods/src/app/app.module.ts
+++ demos/ribbon/methods/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {RibbonModule} from '@smart-webcomponents-angular/ribbon';
-import { ColorPickerModule } from 's@mart-webcomponents-angular/colorpicker';
+import { ColorPickerModule } from '@smart-webcomponents-angular/colorpicker';
 import { InputModule } from '@smart-webcomponents-angular/input';
 import { DropDownListModule } from '@smart-webcomponents-angular/dropdownlist';
 import { AppComponent } from './app.component';
