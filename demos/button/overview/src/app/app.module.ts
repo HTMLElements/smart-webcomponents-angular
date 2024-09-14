5567f2438cd8c01d913934c27f224205181e0c47
bmarkov <boikom@jqwidgets.com>
2024-09-14 18:23:41 +0300

    up

--- demos/button/overview/src/app/app.module.ts
+++ demos/button/overview/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ButtonModule } from '@smart-webcomponents-angular/button';
+import { ProgressBarModule } from '@smart-webcomponents-angular/progressbar';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [ AppComponent ],
-    imports: [ BrowserModule, ButtonModule ],
+    imports: [ BrowserModule, ProgressBarModule, ButtonModule ],
     bootstrap: [ AppComponent ]
 })
 
