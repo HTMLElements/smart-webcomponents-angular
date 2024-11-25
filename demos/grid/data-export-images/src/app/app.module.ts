a15e577efabea3e56bc6e8ed32188caf934a25a6
bmarkov <boikom@jqwidgets.com>
2024-11-25 13:14:47 +0200

    up

--- demos/grid/data-export-images/src/app/app.module.ts
+++ demos/grid/data-export-images/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -12,17 +11,6 @@ import { GridModule } from '@smart-webcomponents-angular/grid';
     schemas: [],
     providers: [],
     bootstrap: [AppComponent]
-=======
-
-import { GridModule } from '@smart-webcomponents-angular/grid';
-
-import { AppComponent } from './app.component';
-
-@NgModule({
-    declarations: [ AppComponent ],
-    imports: [ BrowserModule, GridModule ],
-    bootstrap: [ AppComponent ]
->>>>>>> 620199ab98db9fa8cd0583aaf7cb3ee0405decae
 })
 
 export class AppModule { }
